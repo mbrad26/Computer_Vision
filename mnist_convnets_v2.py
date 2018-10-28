6b0f1b8c73c27ff2527f13366f1ec817efff9a4a
Marius Brad <mbrad26@gmail.com>
2018-10-28 10:07:24 +0000

    ..

---
+++ mnist_convnets_v2.py
@@ -0,0 +1,16 @@
+import os, shutil
+
+original_dataset_dir = 'C:\/Users\mbrad\Downloads\kaggle\dogs-vs-cats/all'
+base_dir = 'C:\/Users\mbrad\Downloads\kaggle\dogs-vs-cats\cats_dogs_small'
+os.mkdir(base_dir)
+
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+train_dogs_dir = os.path.join(train_dir, 'dogs')
