cb01c704970809cfce2fe58adde3fa589f172d3d
Marius Brad <mbrad26@gmail.com>
2018-10-30 18:56:46 +0000

    ...

---
+++ dogs_vs_cats_VGG16_da_v1.py
@@ -0,0 +1,87 @@
+import os
+import matplotlib.pyplot as plt
+from keras.applications import VGG16
+from keras import models
+from keras.layers import Flatten, Dense
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+from time import time
+
+
+start = time()
+
+
+base_dir = 'C:\/Users\mbrad\Downloads\kaggle\dogs-vs-cats\cats_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+conv_base = VGG16(weights='imagenet',
+                  include_top=False,
+                  input_shape=(150, 150, 3))
+conv_base.trainable = False
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    height_shift_range=0.2,
+    width_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50
+)
+
+end = time()
+print('Time Elapsed: ', (end - start) / 60, ' minutes')
+
+history_dict = history.history
+loss = history_dict['loss']
+acc = history_dict['acc']
+val_loss = history_dict['val_loss']
+val_acc = history_dict['val_acc']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Loss')
+plt.plot(epochs, val_loss, 'b', label='Val Loss')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, acc, 'bo', label='Acc')
+plt.plot(epochs, val_acc, 'b', label='Val Acc')
+plt.legend()
+plt.show()
