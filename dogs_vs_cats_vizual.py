b8d8e1cea6286d700837c5002782f2083a54f6df
Marius Brad <mbrad26@gmail.com>
2018-11-01 20:22:25 +0000

    ...

--- dogs_vs_cats_vizual.py
+++ dogs_vs_cats_vizual.py
@@ -25,3 +25,35 @@ first_layer_output = activations[0]
 
 plt.matshow(first_layer_output[0, :, :, 4], cmap='viridis')
 plt.show()
+
+
+layer_names = [layer.name for layer in model.layers[:8]]
+
+images_per_row = 16
+
+for layer_name, layer_activation in zip(layer_names, activations):
+
+    n_features = layer_activation.shape[-1]
+    size = layer_activation.shape[1]
+    n_rows = n_features // images_per_row
+
+    display_grid = np.zeros((size * n_rows, images_per_row * size))
+
+    for row in range(n_rows):
+        for col in range(images_per_row):
+
+            channel_image = layer_activation[0, :, :, row * images_per_row + col]
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+
+            display_grid[row * size: (row + 1) * size, col * size: (col + 1) * size] = channel_image
+
+        scale = 1./size
+
+        plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.imshow(display_grid, aspect='auto', cmap='viridis')
